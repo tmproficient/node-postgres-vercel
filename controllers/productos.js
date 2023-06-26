3bb8dc96910755b1788c833e3fc4e32f60be6a7f
Marcos Zulian <marcoszulian@gmail.com>
2023-06-25 21:20:13 -0300

    actualizando

--- controllers/productos.js
+++ controllers/productos.js
@@ -16,21 +16,3 @@ client.query('SELECT * FROM clientes', (err, res) => {
 
 
 
-
-
-// exports.listAll = (req, res) => {
-//     // console.log("entrando a listALL");
-//     let data = new Array;
-//     const title = req.query.title;
-//     var condition = title ? { title: { [Op.like]: `%${title}%` } } : null;
-//     User.findAll({ where: condition }).then((user) => {
-//         for (let i = 0; i < user.length; i++) {
-//             data.push(user[i].dataValues)
-//         }
-//         let resultado = JSON.parse(JSON.stringify(data));
-//         res.status(200).send({ resultado });
-//     }).catch(error => {
-//         console.log('error ' + error)
-//         res.status(500).send({ error });
-//     })
-// }
