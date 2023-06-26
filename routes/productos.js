effacf71796ee54563438ea2eaf7636875a6c39c
Marcos Zulian <marcoszulian@gmail.com>
2023-06-25 21:34:31 -0300

    actualizando

--- routes/productos.js
+++
@@ -1,8 +0,0 @@
-//Require's
-const express = require("express");
-const router = express.Router();
-
-const productos = require("../controllers/productos");
-router.get("/productos",productos.listAll);
-
-module.exports = router;
